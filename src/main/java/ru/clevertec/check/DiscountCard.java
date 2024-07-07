2c297a3c681722cfc5cc43e9a8746b5f916c9dcc
Anstil <centrumsk8@gmail.com>
2024-07-08 01:12:39 +0300

    Add entities Product and DiscountCard

---
+++ src/main/java/ru/clevertec/check/DiscountCard.java
@@ -0,0 +1,5 @@
+package main.java.ru.clevertec.check;
+
+public record DiscountCard(long id, int number, int discountAmount) {
+
+}
---
+++ src/main/java/ru/clevertec/check/Product.java
@@ -0,0 +1,5 @@
+package main.java.ru.clevertec.check;
+
+public record Product(long id, String description, double price, int quantityInStock, boolean isWholesaleProduct) {
+
+}
